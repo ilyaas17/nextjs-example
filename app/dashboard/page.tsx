fc51e62c3fe91c6f4db582dca08b655afaa25d00
Utkarsh Mishra <ilyaasmis17@gmail.com>
2026-07-07 14:54:14 +0530

    fixed type erroe

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function Page(){
-    return <p>Dashboard Page</p>
+// export default function Page(){
+//     return <p>Dashboard Page</p>
+// }
+
+const Page = () => {
+  return (
+    <p>Dashboard Page</p>
+  )
 }
+
+export default Page;
